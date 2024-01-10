0af20cd898f30fe36529a31913fbc6a31187bace
Prichith C P <126453231+prichith@users.noreply.github.com>
2024-01-10 09:49:32 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,18 @@
+
+
+
+
+// let a=document.createElement("div")
+// a.innerText= "hello world"
+// document.appendChild(a)
+
+// console.log("hello");
+
+// let div=document.getElementById("hello")
+// let newdiv= document.createElement('div')
+// newdiv.innerText = "hello world"
+// div.appendChild(newdiv)
+
+document.getElementById("hai").style.backgroundColor = "yellow"
+// document.getElementById("hello").style.color = "red" 
+document.getElementById("hai").classList.add("hello")
